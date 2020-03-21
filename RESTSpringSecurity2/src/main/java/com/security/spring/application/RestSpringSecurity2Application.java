a76018316713e1671fc36af20f7d7c6697d80203
Priyanka wagh <sonar.priyanka9@gmail.com>
2020-03-21 11:31:56 -0400

    Basic spring security project with three controllers and system provided password

---
+++ RESTSpringSecurity2/src/main/java/com/security/spring/application/RestSpringSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.security.spring.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringSecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringSecurity2Application.class, args);
+	}
+
+}
---
+++ RESTSpringSecurity2/src/test/java/com/security/spring/application/RestSpringSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.spring.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringSecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
